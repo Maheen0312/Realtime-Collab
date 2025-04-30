feb4f3e7578193184df1bec3f9ac1d9a8946ca60
online code collaoration <maheenmohideen@gamil.com>
2025-04-30 19:26:03 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,20 +6,20 @@ import Signup from "./pages/Signup";
 import { SocketProvider } from "./socketContext";
 import './styles/index.css';
 import { io } from "socket.io-client";
-const socket = io(process.env.REACT_APP_API_URL || "");
+const socket = io(process.env.REACT_APP_API_URL || "https://localhost:5000", );
 
 
 function App() {
   return (
     <SocketProvider>
       <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />    
-          <Route path="/signup" element={<Signup />} />   
-          <Route path="/home" element={<Home />} />
-          <Route path="/room/:roomId" element={<Room socket={socket} />} />
-        </Routes>
-      </Router>
+  <Routes>
+    <Route path="/login" element={<Login />} />    
+    <Route path="/signup" element={<Signup />} />   
+    <Route path="/" element={<Home />} />
+    <Route path="/room/:roomId" element={<Room socket={socket} />} />
+  </Routes>
+</Router>
     </SocketProvider>
   );
 }
