04b4095dfcaa6d8fe295381cfb4a42e7eede441a
online code collaoration <maheenmohideen@gamil.com>
2025-05-04 16:18:09 +0530

    updated

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client";
 
 export const initSocket = async () => {
-    const options = {
-        'force new connection': true,
-        reconnectionAttempt: 'Infinity',
-        timeout: 10000,
-        transports: [ 'websocket' ]
-    };
+  const options = {
+    forceNew: true,
+    reconnectionAttempts: Infinity,
+    timeout: 10000,
+    transports: ["websocket"],
+  };
 
-    return io(process.env.REACT_APP_API_URL, options);
+  return io(process.env.REACT_APP_API_URL || "http://localhost:5000", options);
 };
