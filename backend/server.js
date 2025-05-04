e25bec7d5ce95c7dd54970f3d6c17ce52e70bdb0
online code collaoration <maheenmohideen@gamil.com>
2025-05-04 16:37:55 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -64,21 +64,23 @@ function getAllConnectedClients(roomId) {
     });
 }
 io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
-  
-  socket.on("joinRoom", (roomId) => {
+  socket.on("join-room", ({ roomId, user }) => {
     socket.join(roomId);
-    io.to(roomId).emit("userJoined", `${socket.id} has joined the room.`);
+    console.log(`🟢 ${user.name} joined room ${roomId}`);
+  
+    // Emit back to the client to confirm join
+    socket.emit("room-joined", {
+      roomId,
+      user,
+    });
+  
+    // Optionally notify others in the room
+    socket.to(roomId).emit("userJoined", {
+      message: `${user.name} joined the room.`,
+      user,
+    });
   });
-
-  socket.on("leaveRoom", (roomId) => {
-    socket.leave(roomId);
-    io.to(roomId).emit("userLeft", `${socket.id} has left the room.`);
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`User disconnected: ${socket.id}`);
-  });
+  
    // for joining room
    socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
     userSocketMap[ socket.id ] = username;
