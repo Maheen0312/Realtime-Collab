4f7ed9ee2959e86ced176fdc4bd4874e8429ea81
online code collaoration <maheenmohideen@gamil.com>
2025-05-04 19:09:03 +0530

    updated both

--- backend/src/routes/protected.routes.js
+++ backend/src/routes/protected.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../src/middleware/auth.middleware');
+const authMiddleware = require('../middleware/auth.middleware');
 
 // ✅ Protected route
 router.get('/protected', authMiddleware, (req, res) => {
